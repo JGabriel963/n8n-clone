131ebe81e593b7decd83320398ee9e41a9c95424
João Gabriel <joaogabriel9633@gmail.com>
2025-11-27 21:04:29 -0300

    feat: Integrate Sentry for comprehensive error monitoring and performance tracing across client, server, edge, and Inngest functions.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,4 +1,4 @@
-import prisma from "@/lib/db";
+import * as Sentry from "@sentry/nextjs";
 import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
@@ -9,6 +9,14 @@ export const execute = inngest.createFunction(
   { id: "execute-ia" },
   { event: "execute/ia" },
   async ({ event, step }) => {
+    await step.sleep("pretend", "5s");
+
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+    console.warn("Somethins is missing");
+    console.error("This is an error i want to track");
+
     const { steps } = await step.ai.wrap(
       "gemmini-generate-text",
       generateText,
@@ -16,6 +24,11 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant.",
         prompt: "What is 2 + 7?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
